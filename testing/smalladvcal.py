6cef44ee7dfaf4a47fb5c0eb433099c3336e570d
Rayn <94853274+Raynsha@users.noreply.github.com>
2022-03-19 00:07:47 +0700

    Update and rename testing.py to smalladvcal.py

--- testing/smalladvcal.py
+++ testing/smalladvcal.py
@@ -1,4 +1,7 @@
 
+# Not so advanced calculator
+# Use + to add, use - to substract, use * to multiply, use / to divide
+
 num1 = float(input("Enter first number: "))
 op = input("Enter operator: ")
 num2 = float(input("Enter second number: "))
@@ -9,7 +12,7 @@ elif op == "-":
     print(num1 - num2)
 elif op == "x":
     print(num1 * num2)
-elif op == ":":
+elif op == "/":
     print(num1 / num2)
 else:
     print("Invalid operator, please try again")
